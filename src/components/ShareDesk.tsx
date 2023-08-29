60e9e7de5fb9dc7f060d4e732c99e35d7b69bf39
German Pinto <93947560+germanp007@users.noreply.github.com>
2023-08-29 09:54:59 -0300

    Update ShareDesk.tsx

--- src/components/ShareDesk.tsx
+++ src/components/ShareDesk.tsx
@@ -4,7 +4,7 @@ import Twitter from "./Twitter";
 
 const ShareDesk = () => {
   return (
-    <div className="absolute bottom-[90px] right-[-76px] transition ease-in-out delay-150 ">
+    <div className="absolute bottom-[90px] right-[-76px] transition ease-in-out delay-700 ">
       <div className="bg-Very-Dark-Grayish-Blue w-[250px] h-[57px] grid grid-cols-4 rounded-xl px-6  relative after:content-[''] after:absolute after:bottom-[-28px] after:left-1/2 after:transform after:-translate-x-1/2 after:border-transparent after:border-t-Very-Dark-Grayish-Blue after:border-[16px]">
         <div className="col-span-2 flex justify-center items-center ">
           <h1 className="tracking-[0.5em] text-Light-Grayish-Blue text-opacity-[0.4] text-[13px]">
